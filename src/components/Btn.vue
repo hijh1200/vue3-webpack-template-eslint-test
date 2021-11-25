89054af334a26104cb6955073e63c07f0dfff527
hijh1200 <hijh1200@naver.com>
2021-11-26 03:01:11 +0900

    Create Project

---
+++ src/components/Btn.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="text"
+    v-model="msg">
+</template>
+
+
+<script>
+export default {
+ data() {
+   return {
+     msg:''
+   }
+ },
+ emits:[
+   'cm'
+ ],
+ watch:{
+   msg() {
+     this.$emit('cm',this.msg)
+   }
+ }
+}
+</script>
+
+<style>
+  .btn{
+    display: inline-block;
+    margin:4px;
+    padding:6px 12px;
+    color: white;
+    background: gray;
+  }
+</style>
